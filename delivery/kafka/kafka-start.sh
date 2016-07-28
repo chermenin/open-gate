f8dca1330af471cfae6a0ebf2b6651e923bea185
Alex Chermenin <alex@chermenin.ru>
2016-07-28 16:43:40 +0300

    Added Kafka start & stop scripts.

---
+++ delivery/kafka/kafka-start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set KAFKA_HOME=...
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
