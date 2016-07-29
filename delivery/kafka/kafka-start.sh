855d2778932221d390da773b8d6fa4e8af9a1758
Alex Chermenin <alex@chermenin.ru>
2016-07-29 08:37:27 +0300

    Deleted scripts.

--- delivery/kafka/kafka-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set KAFKA_HOME=...
-$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
--- delivery/kafka/kafka-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set KAFKA_HOME=...
-$KAFKA_HOME/bin/zookeeper-server-stop.sh &
-$KAFKA_HOME/bin/kafka-server-stop.sh &
