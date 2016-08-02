1f87adbc23fe112967559535dea72519542b2f3f
Alex Chermenin <alex@chermenin.ru>
2016-08-02 09:43:42 +0300

    Updated KafkaStreamProcessor.

--- core/processing/src/main/java/ru/chermenin/tungsten/processing/KafkaStreamProcessor.java
+++ core/processing/src/main/java/ru/chermenin/tungsten/processing/KafkaStreamProcessor.java
@@ -1,9 +1,11 @@
 package ru.chermenin.tungsten.processing;
 
+import com.continuent.tungsten.replicator.ReplicatorException;
 import com.continuent.tungsten.replicator.applier.RawApplier;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer09;
 import org.apache.flink.streaming.util.serialization.DeserializationSchema;
@@ -11,11 +13,14 @@ import ru.chermenin.kafka.*;
 import ru.chermenin.tungsten.processing.base.StreamProcessor;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 public abstract class KafkaStreamProcessor extends StreamProcessor<KafkaMessage> {
 
-    protected abstract RawApplier getApplier();
+    protected abstract List<RawApplier> getMapAppliers();
+
+    protected abstract List<RawApplier> getSinkAppliers();
 
     protected abstract Properties getKafkaProperties();
 
@@ -44,19 +49,43 @@ public abstract class KafkaStreamProcessor extends StreamProcessor<KafkaMessage>
 
     @Override
     protected MapFunction<KafkaMessage, KafkaMessage> getMap() {
-        final RawApplier applier = getApplier();
         return new MapFunction<KafkaMessage, KafkaMessage>() {
+
+            @Override
             public KafkaMessage map(KafkaMessage message) throws Exception {
-                if (message instanceof DataMessage) {
-                    DataMessage dataMessage = (DataMessage) message;
-                    applier.apply(dataMessage.getEvent(), dataMessage.getHeader(), false, false);
-                } else if (message instanceof CommitMessage) {
-                    applier.commit();
-                } else if (message instanceof RollbackMessage) {
-                    applier.rollback();
-                }
-                return message;
+                return applyMessage(message, getMapAppliers());
             }
         };
+    }
+
+    @Override
+    protected SinkFunction<KafkaMessage> getSink() {
+        return new SinkFunction<KafkaMessage>() {
+
+            @Override
+            public void invoke(KafkaMessage message) throws Exception {
+                applyMessage(message, getSinkAppliers());
+            }
+        };
+    }
+
+    private KafkaMessage applyMessage(KafkaMessage message, List<RawApplier> appliers) throws ReplicatorException, InterruptedException {
+        if (appliers == null) return message;
+
+        if (message instanceof DataMessage) {
+            DataMessage dataMessage = (DataMessage) message;
+            for (RawApplier applier : appliers) {
+                applier.apply(dataMessage.getEvent(), dataMessage.getHeader(), false, false);
+            }
+        } else if (message instanceof CommitMessage) {
+            for (RawApplier applier : appliers) {
+                applier.commit();
+            }
+        } else if (message instanceof RollbackMessage) {
+            for (RawApplier applier : appliers) {
+                applier.rollback();
+            }
+        }
+        return message;
     }
 }
